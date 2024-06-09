c29af8fd1386c2d1b04a6d282eab390c856ac3be
KarenBonillaM <114474341+KarenBonillaM@users.noreply.github.com>
2024-06-09 15:46:11 +0200

    Adds browser router and routes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { useRoutes } from 'react-router-dom';
+import Header from './Components/Header';
+import HomePage from './Pages/HomePage';
+
 
 function App() {
+  const routes = useRoutes([
+    { path: '/', element: <HomePage /> },
+  ]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      {routes}
     </div>
   );
 }
